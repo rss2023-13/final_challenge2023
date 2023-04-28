57d1bbe376f7048ff2b1363691e796374e46ce32
Srinath Mahankali <srinathm@mit.edu>
2023-04-28 18:06:09 -0400

    Hough transform detects lines now

--- phone_johnson_photos/line_detection.py
+++ phone_johnson_photos/line_detection.py
@@ -24,30 +24,44 @@ def image_print(img):
 	cv2.destroyAllWindows()
 
 # read in a test image
-img_num = 6
+img_num = 3
 img_orig = cv2.imread(f"phone_johnson_photos/{img_num}.jpg")
-kernel_width = 3
+img = np.copy(img_orig)
 
+# First blur the image a bit for edge detection purposes
+kernel_width = 5
 kernel = np.ones((kernel_width,kernel_width),np.float32)/(kernel_width*kernel_width)
-img = cv2.filter2D(img_orig,-1,kernel)
+img = cv2.filter2D(img,-1,kernel)
+
+# Next mask out the pixels which are not white since the lanes are approximately white
+# ret,img_masked = cv2.threshold(img,165,255,cv2.THRESH_TOZERO)
+# image_print(img_masked)
 
 # edge detection
 min_thresh = 150
 max_thresh = 200
 img_edges = cv2.Canny(img,min_thresh,max_thresh)
-# image_print(img_edges)
+
+# Cut out the top portion of the image
+mask_portion = 0.7
+mask = np.zeros_like(img_edges)
+mask[int(mask_portion * mask.shape[0]):] = 1
+img_edges *= mask
 
 # Copy edges to the images that will display the results in BGR
-cdst = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2BGR)
+cdst = np.copy(img_orig)
+# cdst = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2BGR)
 cdstP = np.copy(cdst)
 
 # hough transform
 rho = 1
 theta = np.pi/180
 intersect_thresh = 100
+img_width = 5000 # Change this for the car
 lines = cv2.HoughLines(img_edges, rho, theta, intersect_thresh, None, 0, 0)
 # Draw the lines
-if lines is not None:
+draw_lines = True
+if lines is not None and draw_lines:
     for i in range(0, len(lines)):
         rho = lines[i][0][0]
         theta = lines[i][0][1]
@@ -55,8 +69,8 @@ if lines is not None:
         b = np.sin(theta)
         x0 = a * rho
         y0 = b * rho
-        pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-        pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+        pt1 = (int(x0 + img_width*(-b)), int(y0 + img_width*(a)))
+        pt2 = (int(x0 - img_width*(-b)), int(y0 - img_width*(a)))
         cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
 
 cv2.imshow("Source", img)
