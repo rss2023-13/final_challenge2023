2961fc55572b59b9cb13cbaf715bb83a740b947d
Srinath Mahankali <srinathm@mit.edu>
2023-04-29 11:59:46 -0400

    Clean up code and make hough transform work

--- phone_johnson_photos/line_detection.py
+++ phone_johnson_photos/line_detection.py
@@ -23,85 +23,71 @@ def image_print(img):
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-# read in a test image
-img_num = 1
-img_orig = cv2.imread(f"{img_num}.jpg")
-img = np.copy(img_orig)
+def get_lines(img):
+    """
+    Returns the list of lines in the images computed by the Hough transform
+    """
+    # First blur the image a bit for edge detection purposes
+    num_blur_times = 1
+    kernel_width = 11
+    kernel = np.ones((kernel_width,kernel_width),np.float32)/(kernel_width*kernel_width)
+    # kernel = np.ones((kernel_width,kernel_width),np.float32)
+    for i in range(num_blur_times):
+        img = cv2.filter2D(img,-1,kernel)
 
-lower_white = np.array([170, 170, 170])
-upper_white = np.array([255, 255, 255])
+    # Create a mask. Threshold the RGB image to get only the white pixels
+    lower_white = np.array([170, 170, 170])
+    upper_white = np.array([255, 255, 255])
 
-# Create a mask. Threshold the HSV image to get only yellow colors
-img_edges = cv2.inRange(img, lower_white, upper_white)
+    img_edges = cv2.inRange(img, lower_white, upper_white)
 
+    # image_print(img_edges)
 
+    # edge detection
+    min_thresh = 150
+    max_thresh = 200
+    img_edges = cv2.Canny(img_edges,min_thresh,max_thresh)
 
+    # # Cut out the top portion of the image
+    mask_portion = 0.7
+    mask = np.zeros_like(img_edges)
+    mask[int(mask_portion * mask.shape[0]):] = 1
+    img_edges *= mask
 
-# First blur the image a bit for edge detection purposes
-kernel_width = 5
-kernel = np.ones((kernel_width,kernel_width),np.float32)/(kernel_width*kernel_width)
-# kernel = np.ones((kernel_width,kernel_width),np.float32)
-img_edges = cv2.filter2D(img_edges,-1,kernel)
+    # hough transform
+    rho = 1
+    theta = np.pi/180
+    intersect_thresh = 100
+    lines = cv2.HoughLines(img_edges, rho, theta, intersect_thresh, None, 0, 0)
 
-# image_print(img_edges)
+    return lines
 
-# Next mask out the pixels which are not white since the lanes are approximately white
-# ret,img_masked = cv2.threshold(img,165,255,cv2.THRESH_TOZERO)
-# image_print(img_masked)
+def draw_lines(img, lines, img_width):
+    """
+    Draw the lines given by the hough transform
+    """
+    cdst = np.copy(img)
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + img_width*(-b)), int(y0 + img_width*(a)))
+            pt2 = (int(x0 - img_width*(-b)), int(y0 - img_width*(a)))
+            cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+    
+    cv2.imshow("Source", img)
+    cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv2.waitKey()
 
-# edge detection
-min_thresh = 150
-max_thresh = 200
-img_edges = cv2.Canny(img_edges,min_thresh,max_thresh)
+# read in test images for our hough transform
+for i in range(1,8):
+    img_num = i
+    img_orig = cv2.imread(f"phone_johnson_photos/{img_num}.jpg")
 
-# image_print(img_edges)
-
-# # Cut out the top portion of the image
-mask_portion = 0.7
-mask = np.zeros_like(img_edges)
-mask[int(mask_portion * mask.shape[0]):] = 1
-img_edges *= mask
-
-# Copy edges to the images that will display the results in BGR
-cdst = np.copy(img_orig)
-# cdst = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2BGR)
-cdstP = np.copy(cdst)
-
-# hough transform
-rho = 1
-theta = np.pi/180
-intersect_thresh = 100
-img_width = 5000 # Change this for the car
-lines = cv2.HoughLines(img_edges, rho, theta, intersect_thresh, None, 0, 0)
-# Draw the lines
-draw_lines = True
-if lines is not None and draw_lines:
-    for i in range(0, len(lines)):
-        rho = lines[i][0][0]
-        theta = lines[i][0][1]
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        pt1 = (int(x0 + img_width*(-b)), int(y0 + img_width*(a)))
-        pt2 = (int(x0 - img_width*(-b)), int(y0 - img_width*(a)))
-        cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-
-cv2.imshow("Source", img_edges)
-cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
-cv2.waitKey()
-
-# slicing image
-#lower = .5
-#upper = .75
-
-#img[:int(lower*img.shape[0])] = (0,0,0)
-#img[int(upper*img.shape[0]):] = (0,0,0)
-
-#cv2.imshow("slice", img)
-
-
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-# cd_color_segmentation(img)
+    lines = get_lines(img_orig)
+    img_width = 5000 # Change this for the car
+    draw_lines(img_orig, lines, img_width)
