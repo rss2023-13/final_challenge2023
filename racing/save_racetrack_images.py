d487c0cc1d5c1a0584bf88eb9961991af16c7363
Srinath Mahankali <srinathm@mit.edu>
2023-05-01 22:45:47 +0000

    Should save images now

--- racing/save_racetrack_images.py
+++ racing/save_racetrack_images.py
@@ -2,11 +2,13 @@ import os
 import rosbag
 import matplotlib.pyplot as plt
 import math
+import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
 FILE_HEADING = 'johnson_track_lane_4'
-if not os.path.exists('img/' + FILE_HEADING):
-   os.makedirs('img/' + FILE_HEADING)
+img_path = 'img/' + FILE_HEADING
+if not os.path.exists(img_path):
+   os.makedirs(img_path)
 
 BAG_FILE = FILE_HEADING + '.bag'
 print("Bag file is: " + BAG_FILE)
@@ -14,9 +16,13 @@ bag = rosbag.Bag(BAG_FILE)
 bridge = CvBridge() # Converts between ROS images and OpenCV Images
 
 # Save the images
+counter = 0
 for topic, image_msg, time in bag.read_messages(topics=[]):
-    print(time)
-    # img_orig = bridge.imgmsg_to_cv2(image_msg, "bgr8")
+    counter += 1
+    if counter % 50 == 0: # Save every 50th image
+        img_orig = bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        filename = img_path + '/' + time + '.jpg'
+        cv2.imwrite(filename, img_orig)
     # print(img_orig)
 
 bag.close()
