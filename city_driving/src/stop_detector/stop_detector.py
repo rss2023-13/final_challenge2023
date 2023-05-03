7f4eb90bee61381444984c922a823431b9fb4c48
nsthakur <nsthakur@mit.edu>
2023-05-03 16:49:52 -0400

    testing stop sign detector working

--- city_driving/src/stop_detector/stop_detector.py
+++ city_driving/src/stop_detector/stop_detector.py
@@ -5,6 +5,8 @@ import numpy as np
 #from sensor_msgs.msg import Image
 #from geometry_msgs.msg import Point
 from detector import StopSignDetector
+import matplotlib.pyplot as plt
+from IPython.display import display 
 
 class SignDetector:
     def __init__(self):
@@ -13,18 +15,41 @@ class SignDetector:
         #self.subscriber = rospy.Subscriber("/zed/zed_node/rgb/image_rect_color", Image, self.callback)
 
     def callback(self, img_msg):
-        # Process image without CV Bridge
-        np_img = np.frombuffer(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
-        bgr_img = np_img[:,:,:-1]
-        rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+        # TESTING ON CAR:
+        # np_img = np.frombuffer(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
+        # bgr_img = np_img[:,:,:-1]
+        # rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+
+        # is_stopsign, bounding_box = self.detector.predict(rgb_img)
+        # print(is_stopsign, bounding_box)
+
+        # cv2.imshow(self.detector.draw_box(rgb_img, bounding_box))
+
+        #TESTING ON COMPUTER:
+        np_img = img_msg.copy()
+        rgb_img = cv2.cvtColor(np_img, cv2.COLOR_BGR2RGB)
 
         is_stopsign, bounding_box = self.detector.predict(rgb_img)
-        print(is_stopsign, bounding_box)
+        x = cv2.rectangle(np_img, (int(bounding_box[0]), int(bounding_box[1])), (int(bounding_box[2]), int(bounding_box[3])), (255,0,0), 2)
+        
+        image_print(x)
+
+def image_print(img):
+	"""
+	Helper function to print out images, for debugging.
+	Press any key to continue.
+	"""
+	winname = "Image"
+	cv2.namedWindow(winname)        # Create a named window
+	cv2.moveWindow(winname, 40,30)  # Move it to (40,30)
+	cv2.imshow(winname, img)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
 
 
 if __name__=="__main__":
     #rospy.init_node("stop_sign_detector")
-    img_path = "./test_images/stop1.jpeg"
+    img_path = "./test_images/10.png"
     detect = SignDetector()
     detect.callback(img_msg = cv2.imread(img_path))
     #rospy.spin()
